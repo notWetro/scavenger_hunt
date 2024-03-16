ee14f0c99ada0ccf1ee1cf2d8d87432d163668b4
Rosario <aranz.rosario@gmail.com>
2024-03-16 12:43:26 +0100

    Demo: Added Demo Apps for WebApis + Moved Unity-App.

---
+++ demos/TodoApi/TodoApi/DTOs/TodoItemDTO.cs
@@ -0,0 +1,18 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi.DTOs
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public static TodoItemDTO ItemToDTO(TodoItem todoItem) => new()
+           {
+               Id = todoItem.Id,
+               Name = todoItem.Name,
+               IsComplete = todoItem.IsComplete
+           };
+    }
+}
