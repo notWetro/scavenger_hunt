49bee98b89180d27f7ee3b05e315d9215644e4f0
Rosario <aranz.rosario@gmail.com>
2024-03-23 11:37:00 +0100

    Utilities: Added QR-Code-Generator that creates QR-Code from Text.

---
+++ utils/qrcode-generator/App.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using Cocona;
+using QRCG.Core;
+
+CoconaApp.Run(([Option('t')]string text) =>
+{
+    Console.WriteLine("Creating QR-Code from Text.");
+    Console.WriteLine($"Input: '{text}'");
+
+    string downloadsFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads\\";
+    string fileName = "QRCode_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+    string filePath = Path.Combine(downloadsFolder, fileName);
+
+    try
+    {
+        var res = TextQr.FromTextToQr(text, filePath);
+
+        if (res)
+            Console.WriteLine($"Output: '{filePath}'");
+        else
+            Console.WriteLine("Conversion failed.");
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex.Message, "Conversion failed.");
+    }
+});
---
+++ utils/qrcode-generator/QRCG.Core/TextQr.cs
@@ -0,0 +1,20 @@
+﻿using QRCoder;
+
+namespace QRCG.Core
+{
+    public static class TextQr
+    {
+        public static bool FromTextToQr(string text, string path)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+
+            BitmapByteQRCode qrCodeImage = new(qrCodeData);
+            var imageBytes = qrCodeImage.GetGraphic(20);
+
+            File.WriteAllBytes(path, imageBytes);
+
+            return true;
+        }
+    }
+}
