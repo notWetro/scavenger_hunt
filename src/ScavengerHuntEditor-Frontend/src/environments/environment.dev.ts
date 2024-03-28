69447fdaf7a2e28b866c706ed021291a5fc60b77
lukas <code.lks911@gmail.com>
2024-03-28 17:26:35 +0100

    feat: adds template for frontend

---
+++ src/ScavengerHuntEditor-Frontend/src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    environmentStage: "Development",
+    production: true,
+    domain: "",
+    api: {
+        url: "https://api.scavhunt-app.de/api/v1"
+    }
+};
---
+++ src/ScavengerHuntEditor-Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    environmentStage: "Production",
+    production: true,
+    domain: "",
+    api: {
+        url: "https://api.scavhunt-app.de/api/v1"
+    }
+};
