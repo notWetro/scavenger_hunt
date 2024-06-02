b365428aa5b2375efd3224ecbe00fe4150230e8e
Rosario <aranz.rosario@gmail.com>
2024-06-02 19:20:37 +0200

    Feat: Add login-endpoint for participants.

---
+++ src/be-hunt-api/ScavengerHunt.Api/Controllers/ParticipantController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IAuthenticationService = ScavengerHunt.Api.Services.IAuthenticationService;
+
+namespace ScavengerHunt.Api.Controllers
+{
+    public class LoginRequest
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public sealed class ParticipantController(IAuthenticationService authService) : ControllerBase
+    {
+        private readonly IAuthenticationService _authService = authService;
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            var token = await _authService.Authenticate(loginRequest.Username, loginRequest.Password);
+            if (token is null || token == "")
+            {
+                return Unauthorized("Invalid username or password.");
+            }
+
+            return Ok(new { Token = token });
+        }
+
+        // [HttpPost("Logout")] probably not needed rn
+        //public IActionResult Logout()
+        //{
+        //    var token = Request.Headers.Authorization.FirstOrDefault()?.Split(" ").Last();
+        //    if (token == null)
+        //    {
+        //        return BadRequest("Token is missing.");
+        //    }
+
+        //    _authService.Logout(token);
+        //    return Ok("Logged out successfully.");
+        //}
+    }
+}
--- src/be-hunt-api/ScavengerHunt.Api/Program.cs
+++ src/be-hunt-api/ScavengerHunt.Api/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ScavengerHunt.Api.Services;
 using ScavengerHunt.Domain.Repositories;
 using ScavengerHunt.Infrastructure;
 using ScavengerHunt.Infrastructure.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigureServices(builder);
@@ -29,10 +33,36 @@ static void ConfigureServices(WebApplicationBuilder builder)
 
     // Add services
     services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-    services.AddScoped<IHuntRepository, EFHuntRepository>();
-    services.AddScoped<IAssignmentRepository, EFAssignmentRepository>();
-    services.AddScoped<IParticipationRepository, EFParticipationRepository>();
-    services.AddScoped<IParticipantRepository, EFParticipantRepository>();
+    services.AddScoped<IHuntRepository, EFHuntRepository>()
+            .AddScoped<IAssignmentRepository, EFAssignmentRepository>()
+            .AddScoped<IParticipationRepository, EFParticipationRepository>()
+            .AddScoped<IParticipantRepository, EFParticipantRepository>();
+
+    // Add configuration for JWT
+    var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]!);
+    services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
+
+    services.AddScoped<ITokenService, TokenService>()
+            .AddScoped<IAuthenticationService, AuthenticationService>();
 
     // Add other repositories, DbContext, etc.
 
