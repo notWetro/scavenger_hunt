e6185f7713354a4e85d1c97047aabe3e1d052a6f
Rosario <aranz.rosario@gmail.com>
2024-06-06 18:19:35 +0200

    Dev: Rename scav-ar to hunt-game. Feat: Implement User Login at hunt-game.

---
+++ src/fe-hunt-game/Assets/Scripts/HomeScreenController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class HomeScreenController : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField usernameInputField;
+
+    [SerializeField]
+    private TMP_InputField passwordInputField;
+
+
+    [SerializeField]
+    private const string HUNT_API_URL = "http://localhost:7105/api";
+
+    public void OnLoginButtonPressed()
+    {
+        // Update the username and password from the input fields
+        string m_Username = usernameInputField.text;
+        string m_Password = passwordInputField.text;
+
+        Debug.Log($"Pressed Login-Button with Username-Input '{m_Username}'");
+
+        StartCoroutine(LoginRequest(m_Username, m_Password));
+    }
+
+    private IEnumerator LoginRequest(string username, string password)
+    {
+        var formData = new LoginForm { username = username, password = password };
+        string jsonData = JsonUtility.ToJson(formData);
+
+#pragma warning disable IDE0063 // Use simple 'using' statement
+        using (UnityWebRequest request = new UnityWebRequest(HUNT_API_URL + "/Participant/Login", "POST"))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            // Send the request and wait for the response
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError($"Error: {request.error}");
+            }
+            else
+            {
+                Debug.Log($"Response: {request.downloadHandler.text}");
+                if (request.responseCode == 200)
+                {
+                    SceneManager.LoadScene("SampleScene");
+                }
+                else
+                {
+                    Debug.LogError($"Login failed with status code: {request.responseCode}");
+                }
+            }
+        }
+#pragma warning restore IDE0063 // Use simple 'using' statement
+    }
+
+    [System.Serializable]
+    private class LoginForm
+    {
+        public string username;
+        public string password;
+    }
+}
