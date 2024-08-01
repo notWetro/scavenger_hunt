b3bc38931ec0ffd41f28913a1fc07a2cd49fb5d9
lukas <code.lks911@gmail.com>
2024-08-01 15:52:54 +0200

    fix: merge conflict

--- src/be-hunt-api/ScavengerHunt.Api/DTOs/Solution/SolutionGetPrivateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using ScavengerHunt.Domain;
-
-namespace ScavengerHunt.Api.DTOs.Solution
-{
-    public sealed class SolutionGetPrivateDto
-    {
-        public SolutionType Type { get; set; }
-    }
-}
