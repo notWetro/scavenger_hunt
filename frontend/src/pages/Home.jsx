e8f8c2626cee4f23ccfdc81128b7b8466965537c
wetro1 <nik.fichtner52@gmail.com>
2025-07-23 15:00:07 +0200

    changed join button to green

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="home-container">
       <h1>{t("scavenger_hunt")}</h1>
       <div className="button-row">
-        <button className="main-button" onClick={() => navigate("/join")}>{t("join")}</button>
+        <button className="main-button main-button-green" onClick={() => navigate("/join")}>{t("join")}</button>
         <button className="main-button" onClick={() => navigate("/create")}>{t("create")}</button>
       </div>
     </div>
