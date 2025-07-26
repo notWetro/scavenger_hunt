636e9d4cb57e59baa2721416145ed7f475c2804b
wetro1 <nik.fichtner52@gmail.com>
2025-07-26 10:52:32 +0200

    changed folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Hunts from "./pages/Hunts";
 import Navbar from "./components/Navbar";
-import Join from "./pages/Join";
-import StartHunt from "./pages/StartHunt";
-import EditHunt from "./pages/EditHunt";
-import EditQuestion from "./pages/EditQuestion";
+import Join from "./pages/JoinAndPlay/Join";
+import StartHunt from "./pages/JoinAndPlay/StartHunt";
+import EditHunt from "./pages/EditHunt/EditHunt";
+import EditQuestion from "./pages/EditHunt/EditQuestion";
 import "./App.css";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import Login from "./pages/Login/login";
+import Register from "./pages/Login/register";
 
 export default function App() {
   return (
