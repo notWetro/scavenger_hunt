9268d796a89b8ff1d5dac73296098c9068c8e95b
wetro1 <nik.fichtner52@gmail.com>
2025-08-12 10:04:19 +0200

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,8 @@
 const config = {
-    linkOfImage: "https://www.hs-aalen.de/uploads/mediapool/media/file/43045/image_678_hsaa_logo-english-rgb.jpg",
-    infoText: "You need to be logged in to create a hunt. Please log in or register to continue. Or you can view existing hunts without logging in.",
+  linkOfImage:
+    "https://www.hs-aalen.de/uploads/mediapool/media/file/43045/image_678_hsaa_logo-english-rgb.jpg",
+  infoText:
+    "Entwickelt von Hikmet Gözaydin und Niklas Fichtner an der Hochschule Aalen unter der Leitung von Dr. Marc Hermann",
 };
 
 export default config;
